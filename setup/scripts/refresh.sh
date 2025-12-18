b337dac9f6f78e7f89a62da99b7b2ac02fae6a33
khalil hadji <h-adji_tech@proton.me>
2025-12-18 11:02:43 +0100

    fix script bug

--- setup/scripts/refresh.sh
+++ setup/scripts/refresh.sh
@@ -2,6 +2,6 @@
 HOMELAB="$HOME/homelab"
 cd "$HOMELAB" || exit 1
 git pull
-cp -r "$HOMELAB/setup/*" "$HOME/.config"
+cp -r "$HOMELAB"/setup/* "$HOME/.config"
 echo "Applied"
 cd "$HOME/.config/homelab" || exit 1
